5a113263586acfe15231b6aee5d4daec0646d591
jgoralcz <jgoralcz@asu.edu>
2018-10-30 21:22:50 -0700

    added comments for planning.

---
+++ filter_c_files/filter_c_file.py
@@ -0,0 +1,3 @@
+# filter standard c headers that we like: [assert, complex=?, ctype, errno=?, float, limits=?, locale=?, math,
+# filter by a max of 7000 bytes. Or less than 400 lines.
+# malloc, realloc, threads?
