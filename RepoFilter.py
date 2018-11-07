07b29cfd4a138aefbe1a65befe8cae8f04d601af
Zach Monroe <zlmonroe@asu.edu>
2018-11-06 21:54:59 -0700

    RepoStructure now creates things and is flexible if you need to not destroy a folder/file. RepoFilter spelling issues and python standards fixed.

--- RepoFilter.py
+++ RepoFilter.py
@@ -3,19 +3,21 @@ from urllib.parse import quote
 
 from WebNavigator import WebNavigator
 
+
 class RepoFilter:
     """
     First draft of the RepoFilter using the GitHub api. This class searches for repositories on GitHub matching
-    a search. It was written with offline querries in mind as I did not have access to the internet most of the
-    time while writing it. This might be useful in the end for backups / redundency however and provides the
-    ability save a list so that another querry does not have to be made to GitHub and so that results will not
+    a search. It was written with offline queries in mind as I did not have access to the internet most of the
+    time while writing it. This might be useful in the end for backups / redundancy however and provides the
+    ability save a list so that another query does not have to be made to GitHub and so that results will not
     change as repositories change. Using too old of a list might be bad however as the content will not match
     what is being filtered on.
     """
-    #TODO: Review this class for incorrect documentation. It was written in vim because I was on a flight and wanted to conserve battery.
-    #TODO: Review the "online" parts of this class as they have not been thoroughly tested post writing the other parts. (Flight has no Wi-Fi either)
 
-    def __init__(self, search, language=None, blacklist=[], per_page=100):
+    # TODO: Review this class for incorrect documentation. It was written in vim because I was on a flight and wanted to conserve battery.
+    # TODO: Review the "online" parts of this class as they have not been thoroughly tested post writing the other parts. (Flight has no Wi-Fi either)
+
+    def __init__(self, search, language=None, blacklist=None, per_page=100):
         """
         Constructor for RepoFilter. Choose search term string, language string, a blacklist for the descriptions (i.e.
         ignore all repos which contain the phrase "C++" in their descriptions), and the number of results to display
@@ -24,7 +26,7 @@ class RepoFilter:
         @param self:
         @param search: A search string
         @type: str
-        @param langauge: The language to limit searches to
+        @param language: The language to limit searches to
         @type: str
         @param blacklist: A list of terms to disallow in the description
         @type: list
@@ -32,12 +34,14 @@ class RepoFilter:
         @type: int
         """
 
+        if blacklist is None:
+            blacklist = []
         self.search = search
         self.language = language
         self.blacklist = blacklist
         self.per_page = per_page
 
-    def getResults(self, page):
+    def get_results(self, page):
         """
         Makes a single request to the GitGub api for a page with results matching
         the search criteria
@@ -47,47 +51,48 @@ class RepoFilter:
         @type: int
         """
 
-        languageString = ""
+        language_string = ""
         if self.language:
-            languageString = "language=%s&" % (self.language)
+            language_string = "language=%s&" % self.language
 
-        url = "https://api.github.com/legacy/repos/search/%s?%sstart_page=%d" % (quote(self.search), languageString, page)
+        url = "https://api.github.com/legacy/repos/search/%s?%sstart_page=%d" % (
+            quote(self.search), language_string, page)
         print(url)
         content = WebNavigator.getContent(url)
-        jsonContent = json.loads(content)
-        return jsonContent["repositories"]
+        json_content = json.loads(content)
+        return json_content["repositories"]
 
-    def offlineResults(self, filename, startPage, endPage):
+    def offline_results(self, filename, start_page, end_page):
         """
         Saves the list of all repos for offline usage.
         
         @param filename: The name that will be given to the generated file
         @type: str
-        @param startPage: The index of the first page that should be saved
+        @param start_page: The index of the first page that should be saved
         @type: int
-        @param endPage: The index of the last page that should be saved
+        @param end_page: The index of the last page that should be saved
         @type: int
         """
-        #TODO: Test me, I require internet access.
-        #TODO: Consider renaming me when I have a better IDE
+        # TODO: Test me, I require internet access.
+        # TODO: Consider renaming me when I have a better IDE
 
-        allJson = []
-        for i in range(startPage, endPage):
-            allJson += self.getResults(i)
+        all_json = []
+        for i in range(start_page, end_page):
+            all_json += self.get_results(i)
         with open(filename, "w+") as resultsFile:
-            resultsFile.write(json.dumps(allJson))
+            resultsFile.write(json.dumps(all_json))
 
-    def filterRepo(self, repo):
+    def filter_repo(self, repo):
         """
-        Deterime if the given Repo matches our desired criteria. Uses the GitHub info beyond what
+        Determine if the given Repo matches our desired criteria. Uses the GitHub info beyond what
         a simple search has the options of doing.
 
         @param repo: The json from the GitHub repo to filter
         @type: json
         """
-        #TODO: I don't know if "json" is a valid type to put in type for "repo"
-        #TODO: Consider double checking GitHub's language feature
-        
+        # TODO: I don't know if "json" is a valid type to put in type for "repo"
+        # TODO: Consider double checking GitHub's language feature
+
         if any([item in repo["description"] for item in self.blacklist]):
             return False
         if repo["size"] < 1000:
@@ -96,40 +101,41 @@ class RepoFilter:
             return False
         return True
 
-    def getFilteredList(self, unfilteredRepos):
+    def get_filtered_list(self, unfiltered_repos):
         """
         Filters a list of repositories
 
-        @param unfilteredList: The list of unfiltered repositories
+        @param unfiltered_repos: The list of unfiltered repositories
         @tpye list
         """
 
-        return list(filter(self.filterRepo, unfilteredRepos))
+        return list(filter(self.filter_repo, unfiltered_repos))
 
-    def offlineFilteredList(self, filename, unfilteredRepos):
+    def offline_filtered_list(self, filename, unfiltered_repos):
         """
         Filter a list of repositories and save it to JSON for persistent usage
         
         @param filename: The name to save the file as
         @type: str
-        @param unfilteredRepos: The list of repos to filter
-        @type list
+        @param unfiltered_repos: The list of repos to filter
+        @type: list
         """
-        #TODO: Definitely need to rename this method
+        # TODO: Definitely need to rename this method
 
         with open(filename, "w+") as filteredFile:
-            filtered = self.getFilteredList(unfilteredRepos)
-            jsonFiltered = json.dumps(filtered)
-            filteredFile.write(jsonFiltered)
-    
-    def offlineReadJson(self, filename):
+            filtered = self.get_filtered_list(unfiltered_repos)
+            json_filtered = json.dumps(filtered)
+            filteredFile.write(json_filtered)
+
+    @staticmethod
+    def offline_read_json(filename):
         """
         Read in a json file
 
         @param filename: The filename to read
         @type: str
         """
-        #TODO: Check if the json library can do this directly instead of through a read.
+        # TODO: Check if the json library can do this directly instead of through a read.
 
         with open(filename, "r") as resultsFile:
             return json.loads(resultsFile.read())
@@ -140,8 +146,7 @@ if __name__ == "__main__":
 
     switch = input("Which action?")
     if switch == "g":
-        rf.offlineResults("offlineResults.json", 1, 8)
+        rf.offline_results("offlineResults.json", 1, 8)
     elif switch == "f":
-        repos = rf.offlineReadJson("offlineResults.json")
-        rf.offlineFilteredList("filteredOfflineResults.json", repos)
-
+        repos = rf.offline_read_json("offlineResults.json")
+        rf.offline_filtered_list("filteredOfflineResults.json", repos)
--- RepoStructure.py
+++ RepoStructure.py
@@ -1,21 +1,55 @@
 import os
+import json
+import datetime
 
 
 class RepoStructure:
-    def __init__(self, parentDir="."):
-        self.parentDir = parentDir
-        if os.path.exists(os.path.join(parentDir, "Repositories")):
-            # TODO: Create "Repositories" folder in parentDir
-            pass
+    def __init__(self, parent_dir="."):
+        self.parentDir = parent_dir
+        self.root = os.path.join(parent_dir, "Repositories")
+        RepoStructure.__mkdir(self.root)
+
+    def format_repo(self, repo_json, filter_date):
+        name_string = repo_json["username"] + "/" + repo_json["name"]
+        repo_dir = os.path.join(self.root, name_string)
+        RepoStructure.__mkdir(repo_dir)
+        meta_dir = os.path.join(repo_dir, "Config.META")
+        RepoStructure.__echo(meta_dir, RepoStructure.__get_meta_inf(repo_json, filter_date))
 
     @staticmethod
-    def formatRepo(self, repo):
-        # TODO: Create repo folder inside "Repositories" folder
-        # TODO: Create metafile in repo folder
-        pass
+    def __get_meta_inf(repo, filter_date):
+        # TODO: Get license info
+        meta_file = {
+            "url": "%s" % (repo["url"]),
+            "author": "%s" % (repo["username"]),
+            "filter-date": "%s" % filter_date
+        }
+        return json.dumps(meta_file)
+
+    @staticmethod
+    def __mkdir(folder, clobber=False):
+        # Uses lazy evaluation
+        if clobber or not os.path.exists(folder):
+            os.mkdir(folder)
+            return True
+        return False
+
+    @staticmethod
+    def __echo(filename, string="", clobber=False):
+        # Uses lazy evaluation
+        if clobber or not os.path.exists(filename):
+            with open(filename, "w+") as file:
+                file.write(string)
+            return True
+        return False
 
 
 if __name__ == "__main__":
     from RepoFilter import RepoFilter
 
     rs = RepoStructure()
+    rf = RepoFilter("C ", language="C", blacklist=["C++", "C#"], per_page=1)
+    rf.offline_results("offlineResults.json", 1, 1)
+    repos = rf.offline_read_json("offlineResults.json")
+    rf.offline_filtered_list("filteredOfflineResults.json", repos)
+    filteredRepos = rf.offline_read_json()
