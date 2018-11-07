c22c4ad2a3a5e44a0207380090fdf4cee2df5de0
Zach Monroe <zlmonroe@asu.edu>
2018-11-06 23:25:49 -0700

    Tried to make structure more consistent and hopefully fix JJ's import after, checking with Travis....

--- tests/test_GitHubNavigator/test_US58.py
+++ tests/test_GitHubNavigator/test_US58.py
@@ -1,7 +1,7 @@
 import os
 import time
 import unittest
-from GitHubScraper import GitHubScraper
+from DataGathering.GitHubScraper import GitHubScraper
 import shutil
 
 
--- tests/test_placement.py
+++
@@ -1,6 +0,0 @@
-# this test case is needed until there is at least 1 other test case.
-# Otherwise we will get an exit code of '5' and travis will reject.
-
-
-def test_placeholder():
-    pass
