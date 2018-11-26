b8abcba2b4dc41312de56c8d021a7c7db1125f60
jgoralcz <jgoralcz@asu.edu>
2018-11-25 22:38:06 -0700

    initial start of combining the classes.

--- decompy/DataGathering/GatherData.py
+++ decompy/DataGathering/GatherData.py
@@ -1,7 +1,20 @@
-from decompy.DataGathering.RepoFilter import RepoFilter as rf
-from decompy.DataGathering.GitHubScraper import GitHubScraper as ghscraper
-from decompy.DataGathering.FilterC import FilterC as fc
-from decompy.DataGathering.ClangSubprocess import Clang as clang
+from decompy.DataGathering.RepoFilter import RepoFilter
+from decompy.DataGathering.RepoStructure import RepoStructure
+from decompy.DataGathering.GitHubScraper import GitHubScraper
+from decompy.DataGathering.FilterC import FilterC
+from decompy.DataGathering.ClangSubprocess import Clang
+
+# from decompy.DataGathering.RepoFilter import RepoFilter
+#     import datetime
+#
+#     rf = RepoFilter("C ", language="C", blacklist=["C++", "C#"])
+#     rf.offline_results("offlineResults.json", 1, 2)
+#     repos = rf.offline_read_json("offlineResults.json")
+#     rf.offline_filtered_list("filteredOfflineResults.json", repos)
+#     filteredRepos = rf.offline_read_json("filteredOfflineResults.json")
+#
+#     rs = RepoStructure()
+#     rs.batch_format(filteredRepos, datetime.datetime.now())
 
 
 class GatherData:
@@ -12,3 +25,15 @@ class GatherData:
     """
 
     def __init__(self):
+        """
+        initiliazes a new object, containing the other classes, one to rule them all.
+        """
+        self.rf = RepoFilter("C ", language="C", blacklist=["C++", "C#", "css"], per_page=100)  # gets 100 results
+        self.rs = RepoStructure()
+        self.ghs = GitHubScraper()
+        self.fc = FilterC()
+        self.clang = Clang()
+
+
+    def hundred(self, ):
+
