3770681b6239b7102af6bf8fa63f5c7234a3b7e9
jgoralcz <jgoralcz@asu.edu>
2018-11-26 12:09:58 -0700

    beggining the stage processes.

--- decompy/DataGathering/__init__.py
+++ decompy/DataGathering/__init__.py
@@ -2,5 +2,6 @@ from decompy.DataGathering.GitHubScraper import GitHubScraper
 from decompy.DataGathering.RepoFilter import RepoFilter
 from decompy.DataGathering.RepoStructure import RepoStructure
 from decompy.DataGathering.ClangSubprocess import Clang
+from decompy.DataGathering.FilterC import FilterC
 
-__all__ = ["Clang", "GitHubScraper", "RepoFilter", "RepoStructure"]
+__all__ = ["GitHubScraper", "RepoFilter", "RepoStructure", "Clang", "FilterC"]
