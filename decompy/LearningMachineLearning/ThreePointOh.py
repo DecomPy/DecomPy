d13c9903ede1fa3d28915bf326a152ccd5fb34e3
yzgarrard <yzgarrad@gmail.com>
2019-01-24 19:28:49 -0700

    Converted keras tutorial using fashion to using normal mnist. ~97% accuracy with minimal changes

--- decompy/LearningMachineLearning/ThreePointOh.py
+++ decompy/LearningMachineLearning/ThreePointOh.py
@@ -7,22 +7,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Load data
-fashion_mnist = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+mnist = keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+train_images = train_images.flatten()
 
 # Convenient labels for later
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+class_names = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
 
 # Scale down values to between 0 and 1
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
 model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),  # Converts 28x28 input (2D array) to a 1D 784 pixel input
+    keras.layers.Flatten(input_shape=(784, 1)),  # 785 feature input layer
     keras.layers.Dense(128, activation=tf.nn.relu),  # Fully connected layers of 128 nodes
     keras.layers.Dense(10, activation=tf.nn.softmax)    # output layer of 10 nodes where values sum up to 1
 ])
+
+keras.layers.Fl
 
 # Some more settings for the NN
 model.compile(optimizer=tf.train.AdamOptimizer(),
