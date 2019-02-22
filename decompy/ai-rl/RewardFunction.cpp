9e081c9d6daad5cc637926da5030d4c0824a62a1
vcedgar <vcedgar@asu.edu>
2019-02-21 18:51:41 -0700

    Can't get LLVM stuff to compile

--- decompy/ai-rl/RewardFunction.cpp
+++ decompy/ai-rl/RewardFunction.cpp
@@ -1,2 +1,31 @@
 #include "llvm/IR/Function.h"
 
+using namespace llvm;
+
+class Reward{
+
+    public:
+    static int getReward(Function fnc1, Function fnc2);
+
+    private:
+    Function fnc1;
+    Function fnc2;
+
+    static int instructionCountReward();
+    static int identicalInstructionCountReward();
+
+}
+
+static int Reward::getReward(Function fnc1, Function fnc2){
+
+    return 0;
+}
+
+static int instructionCountReward(){
+
+    return 0;
+}
+
+static int identicalInstructionCountReward(){
+    return 0;
+}
