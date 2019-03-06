0f9d3590819451bda764bf9269061d0d05b4d017
jgoralcz <jgoralcz@asu.edu>
2019-03-06 16:09:10 -0700

    Action generator finished. Needs review.

---
+++ decompy/RL/ActionGenerator/OptimizationLister.py
@@ -0,0 +1,25 @@
+from decompy.RL.Action import PassAction
+from decompy.RL.ActionGenerator import ActionGenerator
+
+
+# TODO: decide if I should be extending, then override the method or not? The class diagram shows the same method.
+class OptimizationLister(ActionGenerator.ActionGenerator):
+    """
+    Step 2.1 The OptimizationLister generates a list of optimization based actions.
+    """
+
+    def __init__(self):
+        super(ActionGenerator, self).__init__()
+        pass
+
+    def list_optimizations_actions(self, llvm_unop):
+        """
+        The driver calls the ActionGenerator to get a list of possible actions for the current
+        LLVM state it received in step 1.
+        :param llvm_unop: the unoptimized llvm
+        :type: string
+        :return: list of actions
+        :rtype: list<Action>
+        """
+        pass
+
