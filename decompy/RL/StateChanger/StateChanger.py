fd19acb14166daee955f504ba878eefb422f7ebd
jgoralcz <jgoralcz@asu.edu>
2019-03-06 17:22:53 -0700

    StateChanger stubs added to model.

---
+++ decompy/RL/StateChanger/StateChanger.py
@@ -0,0 +1,23 @@
+# TODO: import statements
+# TODO: decide which things need to be static.
+
+
+class StateChanger:
+    """
+    Step 4: Gets the Action from step 3 and generates new LLVM based off that action, which is the new state.
+    """
+
+    def __init__(self):
+        """
+        Initializes the class.
+        """
+        pass
+
+    def change_state(self, llvm):
+        """
+        Takes the Action from step 3 and generates new LLVM based off that action.
+        :param: llvm
+        :type: str
+        """
+        pass
+
