45b2ff2ee65a76ecb1cd9892eb02e0b4425f5457
jgoralcz <jgoralcz@asu.edu>
2019-03-13 15:23:11 -0700

    read in the file.

--- decompy/EquivalencyClasses/Snippet.py
+++ decompy/EquivalencyClasses/Snippet.py
@@ -6,28 +6,18 @@ class Snippet:
     A snippet representation from the database.
     """
 
-    def __init__(self, id, llvm, class_id):
+    def __init__(self, id, class_id, llvm):
         """
         the content of a snippet.
         :param id: the id of the snippet.
         :type: integer
-        :param llvm: the llvm snippet
-        :type: string
         :param class_id: id of class
         :type: integer
+        :param llvm: the llvm snippet
+        :type: string
         """
         self.id = id
-        self.llvm = llvm
         self.class_id = class_id
-        pass
-
-    def __eq__(self, other):
-        """
-        Determines the equality of this snippet when compared to another snippet
-        :param other: the other snippet
-        :type: Snippet
-        :return: True or False based off of equality.
-        :rtype: bool
-        """
+        self.llvm = llvm
         pass
 
--- decompy/EquivalencyClasses/SnippetRepository.py
+++ decompy/EquivalencyClasses/SnippetRepository.py
@@ -1,3 +1,6 @@
+import os
+import errno
+
 from decompy.EquivalencyClasses import Snippet
 
 
@@ -7,13 +10,43 @@ class SnippetRepository:
     """
 
     def __init__(self):
+        self.snippets = []
+        self.llvm_path = ".ll"
         pass
 
     def get_snippets(self):
         """
-        Gets the list of snippets
+        Gets the list of snippets that it has read in.
         :return: snippets
         :rtype: list<Snippets>
         """
-        pass
+        return self.snippets
+
+    def read_snippets_from_file(self, file_path):
+        """
+        reads snippets from destinated file path, only gets .ll files.
+        :param: file_path the file path to read from.
+        :type: str
+        :return:
+        """
+        # loop through all files from given path
+        if not os.path.exists(file_path):
+            # example output [Errno 2] No such file or directory: 'your file path'
+            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), file_path)
+
+        # walk recursively in given folder
+        for root, dirs, files in os.walk(file_path):
+            try:
+                
+
+            except Exception as e:
+                print("Could not find the file...", e)
+                pass
+
+        return self.snippets
+
+    # look for unfiltered files and only want "Unfiltered" (or filt_path_name)
+
+
+
 
