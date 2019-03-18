846e84848721f9bd02cc2802cd2e82d7f719825d
yzgarrard <yzgarrard@gmail.com>
2019-03-18 11:43:35 -0700

    Fixed tokenizing for Modules from .ll files

--- decompy/EquivalencyClasses/Tokenizers/CharacterTokenizer.py
+++ decompy/EquivalencyClasses/Tokenizers/CharacterTokenizer.py
@@ -60,20 +60,11 @@ class CharacterTokenizer:
 
 
 if __name__ == "__main__":
-    # s = Snippet(0, "%1 = alloca i32, align 4\n%2 = alloca i32, align 4", 0)
-    # print(CharacterTokenizer.tokenize(s, True))
-
-    # m = "; ModuleID = 'example.cpp'\nsource_filename = \"example.cpp\"\ntarget datalayout = \"e-m:e-i64:64-f80:128-n8:16:32:64-S128\"\ntarget triple = " \
-    #     "\"x86_64-pc-linux-gnu\"\n\n; Function Attrs: noinline\nnounwind optnone uwtable\ndefine dso_local i32 @_Z7examplei(i32) #0 {\n %2 = alloca i32, " \
-    #     "align 4\n %3 = alloca i32, align 4\n store i32 %0, i32* %2, align 4\n %4 = load i32, i32* %2, align 4\n %5 = add nsw i32 %4, 3\n store i32 %5, " \
-    #     "i32* %3, align 4\n %6 = load i32, i32* %3, align 4\n ret i32 %6\n}\nattributes #0 = { noinline nounwind optnone uwtable " \
-    #     "\"correctly-rounded-divide-sqrt-fp-math\"=\"false\" \"disable-tail-calls\"=\"false\" \"less-precise-fpmad\"=\"false\" " \
-    #     "\"min-legal-vector-width\"=\"0\" \"no-frame-pointer-elim\"=\"true\" \"no-frame-pointer-elim-non-leaf\" \"no-infs-fp-math\"=\"false\" " \
-    #     "\"no-jump-tables\"=\"false\" \"no-nans-fp-math\"=\"false\" \"no-signed-zeros-fp-math\"=\"false\" \"no-trapping-math\"=\"false\" " \
-    #     "\"stack-protector-buffer-size\"=\"8\" \"target-cpu\"=\"x86-64\" \"target-features\"=\"+fxsr,+mmx,+sse,+sse2,+x87\" \"unsafe-fp-math\"=\"false\" " \
-    #     "\"use-soft-float\"=\"false\" }\n\n!llvm.module.flags = !{!0}\n!llvm.ident = !{!1}\n\n!0 = !{i32 1, !\"wchar_size\", i32 4}\n!1 = !{!\"clang version " \
-    #     "8.0.0-svn354892-1~exp1~20190226195658.47 (branches/release_80)\"} "
+    s = Snippet(0, "%1 = alloca i32, align 4\n%2 = alloca i32, align 4", 0)
+    print(CharacterTokenizer.tokenize(s, True))
 
     m = "define i32 @mul_add(i32 %x, i32 %y, i32 %z) {\nentry:\n  %tmp = mul i32 %x, %y\n  %tmp2 = add  i32 %tmp, %z\n  ret i32 %tmp2\n}"
+    with open("example.ll") as f:
+        m = f.read()
 
     print(CharacterTokenizer.tokenize(m, False))
--- decompy/EquivalencyClasses/Tokenizers/RunMeInThisDirectory.sh
+++ decompy/EquivalencyClasses/Tokenizers/RunMeInThisDirectory.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-file="$1"
-
 clang++-8 -g ExtractInstructions.cpp `llvm-config-8 --cxxflags --ldflags --libs core` -lpthread -o ExtractInstructions.out
-./ExtractInstructions.out
+./makeSO.sh
+#./ExtractInstructions.out
