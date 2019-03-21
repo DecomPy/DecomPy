8cc9f1b685718a52b07c2f5f50ca98faef9a3d36
jgoralcz <jgoralcz@asu.edu>
2019-03-21 14:05:33 -0700

    added abstract class, additional documentation and __main__ methods for implemented Actions. Important: do_actions are still not implemented and require more documentation.

--- decompy/RL/Action/SwapAction.py
+++ decompy/RL/Action/SwapAction.py
@@ -1,7 +1,7 @@
-from decompy.RL.Action import Action
+from decompy.RL.Action.Action import Action
 
 
-class SwapAction(Action.Action):
+class SwapAction(Action):
     """
     Data Structure for an Action, particularly when a swap is performed.
     """
@@ -13,4 +13,17 @@ class SwapAction(Action.Action):
         """
         super(SwapAction, self).__init__()
         self.swap_id = swap_id
+
+    def do_action(self):
+        """
+        an action performed based off swap action.
+        TODO: implementer add more info
+        :return:
+        """
         pass
+
+
+# test to see it if runs
+if __name__ == "__main__":
+    pa = SwapAction(1)
+    pa.do_action()
