af864c8d1d20e1ce02095f14f729b71d0c515de3
yzgarrard <yzgarrard@gmail.com>
2019-03-26 19:01:03 -0700

    made pass actions more abstract

--- decompy/RL/ActionGenerator/OptimizationLister.py
+++ decompy/RL/ActionGenerator/OptimizationLister.py
@@ -1,4 +1,3 @@
-from decompy.RL.Action.PassAction import PassAction
 
 
 class OptimizationLister:
@@ -19,34 +18,9 @@ class OptimizationLister:
         :return: list of pass actions
         :rtype: list<PassAction>
         """
-        return_list = []
-
-        # The following are for opt optimizations
-        opt_optimizations = ["-adce", "-always-inline", "-argpromotion", "-loop-vectorize", "-break-crit-edges", "-codegenprepare",
-                             "-constmerge", "-constprop", "-dce", "-deadargelim", "-die", "-dse", "-functionattrs", "-globaldce",
-                             "-globalopt", "-gvn", "-indvars", "-inline", "-instcombine", "-aggressive-instcombine", "-internalize", "-ipconstprop",
-                             "-ipsccp", "-jump-threading", "-lcssa", "-licm", "-loop-deletion", "-loop-extract", "-loop-extract-single", "-loop-reduce",
-                             "-loop-rotate", "-loop-simplify", "-loop-unroll", "-loop-unroll-and-jam", "-loop-unswitch", "-loweratomic", "-lowerinvoke",
-                             "-lowerswitch", "-mem2reg", "-memcpyopt", "-mergefunc", "-mergereturn", "-partial-inliner", "-prune-eh", "-reassociate",
-                             "-reg2mem", "-sroa", "-sccp", "-simplifycfg", "-sink", "-strip", "-strip-dead-debug-info", "-strip-dead-prototypes",
-                             "-strip-debug-declare", "-strip-nondebug", "-tailcallelim"]
-        # Optimizations that are not included:
-        for opt in opt_optimizations:
-            pa = PassAction(
-                "opt_" + opt[1:],
-                "opt " + opt + " -S output" + opt + ".ll -o output" + opt + "_passed.ll",
-                llvm_module_string=llvm_unop,
-                intermediate_file="output" + opt + ".ll",
-                output_file="output" + opt + "_passed.ll")
-            pa.requires_llvm_module = True
-            return_list.append(pa)
-
-        return return_list
+        return []
 
 
 # test to see it if runs
 if __name__ == "__main__":
-    optimization_list = OptimizationLister.list_optimizations_actions(
-        "define i32 @mul_add(i32 %x, i32 %y, i32 %z) {\nentry:\n  %tmp = mul i32 %x, %y\n  %tmp2 = add  i32 %tmp, %z\n  ret i32 %tmp2\n}")
-    for optimization in optimization_list:
-        optimization.do_action()
+    pass
