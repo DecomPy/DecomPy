0cfd6335b0a50533bb838191b5cc59d7f60d470e
zlmonroe <zlmonroe@asu.edu>
2019-04-06 00:16:46 -0700

    Created the decision. It should not be mutable which should help keep the model safe. I provided some convenience functions for the model as well.

--- decompy/RL/Model/Decision.py
+++ decompy/RL/Model/Decision.py
@@ -1,3 +1,4 @@
+import copy
 
 class Decision:
     """
@@ -14,8 +15,66 @@ class Decision:
         :param action: the Action this decision performs.
         :type: Action
         """
-        self.summary = summary
-        self.reward = reward
-        self.action = action
-        pass
+        self._summary = summary
+        self._reward = reward
+        self._action = action
+
+    def related_state(other):
+        """
+        Determines whether the other Decision occured from
+        a similar state by checking if the summary of their
+        states was equal. This operation is faster than checking
+        the summaries yourself since it does not make any copies.
+
+        :param other: The other Decision object
+        :type: Decision
+        """
+        if isinstance(other, Decision):
+            return self._summary == other._summary
+        return False
+
+    def same_action(other):
+        """
+        Check if the action is the same for two decisions (of
+        possibly different llvm summary states)
+        """
+        if isinstance(other, Decision):
+            return self._action == other._action
+        return False
+
+    def get_summary():
+        """
+        Creates a deep copy of the summary object to avoid
+        unwanted mutations to the model. Be careful to avoid
+        repeated calls since this will be expensive.
+        """
+        return copy.deepcopy(self._summary)
+
+    def get_reward():
+        """
+        Gets the reward. No copying is needed for an int
+        """
+        return reward
+
+    def get_action():
+        """
+        Creates a deep copy of the summary object to avoid
+        unwanted mutations to the model. Be careful to avoid
+        repeated calls since this will be expensive.
+        """
+        return copy.deepcopy(self._action)
+
+    def __lt__(other):
+        """
+        Orders the decisions by their rewards. Useful for choosing
+        the best choice.
+        """
+        return self._reward < other.reward
+
+    def __gt__(other):
+        """
+        Orders the decisions by their rewards. Useful for choosing
+        the best choice.
+        """
+        return self._reward > other.reward
 
