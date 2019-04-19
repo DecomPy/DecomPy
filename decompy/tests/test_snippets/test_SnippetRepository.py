07f0cc08adf5c7aa09c43749762df4d2ed291eb3
vcedgar <vcedgar@123theLaptop.localdomain>
2019-04-19 15:29:39 -0700

    fixing tests

--- decompy/tests/test_snippets/test_SnippetRepository.py
+++ decompy/tests/test_snippets/test_SnippetRepository.py
@@ -23,25 +23,25 @@ class TestRepoStructure(unittest.TestCase):
             # first one
             if snippets[i][0] == "class1/1.ll":
                 # self.assertTrue(snippets[i][0] == "Class1/1.ll")
-                self.assertTrue(snippets[i][2] == "Class1")
+                self.assertTrue(snippets[i][2] == "class1")
                 self.assertTrue(snippets[i][1] == "; ModuleID = 1.ll\n; test")
 
             # second
             if snippets[i][0] == "class1/2.ll":
                 # self.assertTrue(snippets[i][0] == "Class1/2.ll")
-                self.assertTrue(snippets[i][2] == "Class1")
+                self.assertTrue(snippets[i][2] == "class1")
                 self.assertTrue(snippets[i][1] == "; ModuleID = 2.ll")
 
             # third
             if snippets[i][0] == "class2/1.ll":
                 # self.assertTrue(snippets[i][0] == "Class2/1.ll")
-                self.assertTrue(snippets[i][2] == "Class2")
+                self.assertTrue(snippets[i][2] == "class2")
                 self.assertTrue(snippets[i][1] == "; ModuleID = 1.ll")
 
             # fourth
             if snippets[i][0] == "class2/2.ll":
                 # self.assertTrue(snippets[i].id == "Class2/2.ll")
-                self.assertTrue(snippets[i][2] == "Class2")
+                self.assertTrue(snippets[i][2] == "class2")
                 self.assertTrue(snippets[i][1] == "; ModuleID = 2.ll")
 
     def test_snippet_repository(self):
