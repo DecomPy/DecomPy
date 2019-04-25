7fd81b3455884cf553e38f7995b5d32ee9a4220c
zlmonroe <zlmonroe@asu.edu>
2019-04-24 17:26:17 -0700

    Got reward and goal state added in (don't work, seg fault) and added readme for UI

--- decompy/demo/actiondemo.py
+++ decompy/demo/actiondemo.py
@@ -1,6 +1,9 @@
 import tkinter
+from tkinter import ttk
 
 from decompy.RL.ActionGenerator.ActionGenerator import ActionGenerator
+from decompy.EquivalencyClasses.Tokenizers.Tokenizer import Tokenizer
+
 from decompy.demo.actionlist import ActionList
 from decompy.demo.ChangeText import ChangeText
 
@@ -13,14 +16,46 @@ class ActionDemo(tkinter.Frame):
 
         self.actionGen = ActionGenerator()
 
-        self.text1 = ChangeText(self)
-        self.text1.pack(side=tkinter.LEFT, expand=True, fill="both")
-        self.text1.bind_all("<<Change>>", self.should_update)
+        # Input Notebook with plain text module input and goal input
+        input_notebook = ttk.Notebook(self)
 
-        self.text2 = tkinter.Text(self)
-        self.text2.pack(side=tkinter.RIGHT, expand=True, fill="both")
+        input_page = tkinter.Frame(input_notebook)
+        goal_page = tkinter.Frame(input_notebook)
 
-        self.actionList = ActionList(self, self.text1, self.text2)
+        self.input = ChangeText(input_page)
+        self.input.pack(expand=True, fill="both")
+        self.input.bind_all("<<Change>>", self.should_update)
+
+        self.goal_state = tkinter.Text(goal_page)
+        self.goal_state.pack(expand=True, fill="both")
+
+        input_notebook.add(input_page, text="LLVM Module")
+        input_notebook.add(goal_page, text="Goal State")
+
+        input_notebook.pack(side=tkinter.LEFT, expand=1, fill="both")
+
+        # Output Notebook with tokenizer, action execution, reward calculation
+        output_notebook = ttk.Notebook(self)
+
+        token_page = tkinter.Frame(output_notebook)
+        action_page = tkinter.Frame(output_notebook)
+        reward_page = tkinter.Frame(output_notebook)
+
+        self.tokenizer_text = tkinter.Text(token_page)
+        self.action_text = tkinter.Text(action_page)
+        self.reward_entry = tkinter.Entry(reward_page)
+
+        self.action_text.pack(expand=True, fill="both")
+        self.tokenizer_text.pack(expand=True, fill="both")
+        self.reward_entry.pack(side=tkinter.TOP, fill="x")
+
+        output_notebook.add(token_page, text='Tokenizer')
+        output_notebook.add(action_page, text='Action Execution')
+        output_notebook.add(reward_page, text="Reward")
+
+        output_notebook.pack(side=tkinter.RIGHT, expand=1, fill="both")
+
+        self.actionList = ActionList(self, self.input, self.action_text, self.reward_entry, self.goal_state)
         self.actionList.pack(side=tkinter.LEFT, fill="both", expand=True)
 
     def should_update(self, e):
@@ -29,7 +64,13 @@ class ActionDemo(tkinter.Frame):
         :param e: Event
         """
         def update():
-            llvm_state = self.text1.get("0.0", tkinter.END)
+            llvm_state = self.input.get("0.0", tkinter.END)
+
+            result = Tokenizer.tokenize(llvm_state, False)
+            meta, _, _, _, _ = Tokenizer.extract_meta_tokens(result)
+            self.tokenizer_text.delete("1.0", tkinter.END)
+            self.tokenizer_text.insert("1.0", " ".join(["(%s)" % str(t) for t in meta]))
+
             self.actionList.updateActions(
                 self.actionGen.list_actions(llvm_state),
                 llvm_state
--- decompy/demo/actionlist.py
+++ decompy/demo/actionlist.py
@@ -1,14 +1,17 @@
 import tkinter
 
 from decompy.demo.highlightText import highlightDiffs, configureHighlightColors
+from decompy.RL.Reward.RewardFunction import RewardFunction
 
 
 class ActionList(tkinter.Frame):
-    def __init__(self, parent, input_textbox, output_textbox, active_color="red"):
+    def __init__(self, parent, input_textbox, output_textbox, reward, goal_text, active_color="red"):
         tkinter.Frame.__init__(self, parent)
         self.actionList = []
         self.input_textbox = input_textbox
         self.output_textbox = output_textbox
+        self.goal_textbox = goal_text
+        self.reward_entry = reward
         configureHighlightColors(self.input_textbox, self.output_textbox)
         self.active_color = active_color
 
@@ -42,3 +45,22 @@ class ActionList(tkinter.Frame):
         self.output_textbox.delete("1.0", tkinter.END)
         self.output_textbox.insert("1.0", action.do_action(llvm_state))
         highlightDiffs(self.input_textbox, self.output_textbox)
+
+        in_state = self.input_textbox.get("1.0", "end")
+        out_state = self.output_textbox.get("1.0", "end")
+        goal_state = self.goal_textbox.get("1.0", "end")
+
+        if out_state.strip() != "":
+            reward = RewardFunction().create_reward(in_state, out_state, goal_state)
+
+            effect = "no"
+            if reward > 0:
+                effect = "a positive"
+            elif reward < 0:
+                effect = "a negative"
+
+            self.reward_entry.delete(0, "end")
+            self.reward_entry.insert(0, "The calculated reward was %d. "
+                                        "This means that this action had %s effect on the llvm code" %
+                                     (reward, effect))
+
